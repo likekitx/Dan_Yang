ad7431ecf1919f710066f2f73ed05b422edd1d43
kitten <2721393058@qq.com>
2022-11-10 18:31:44 +0800

    提交了

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import { join } from 'path'
+import Vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [
+    Vue(),
+  ],
+  base:'/',
+  resolve:{
+    alias:{
+      '@': join(__dirname, "src"),
+    }
+  },
+  server: {
+    host: 'relinex.cn',
+    port: 801,
+    proxy: {
+      "/wx": {
+        target: "http://relinex.cn:3741",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/wx/, "")
+      },
+    },
+  },
+  define: {
+    'process.env': {}
+  }
+})
